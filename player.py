9f64a75b010d331882187e0005d0a4df044914de
a s <diamihaianuc@icloud.com>
2020-07-23 15:45:18 +0300

    update 1

---
+++ player.py
@@ -0,0 +1,11 @@
+### PLAYER ###
+player = {}
+
+# HP
+player['HP'] = {}
+player['HP']['health'] = 55
+player['HP']['maxHealth'] = 55
+player['HP']['healthDashes'] = 31
+player['HP']['dashConvert'] = int(player['HP']['maxHealth']/player['HP']['healthDashes'])
+player['HP']['currentDashes'] = int(player['HP']['health']/player['HP']['dashConvert'])
+
